b6ffeb8bdeb0f5355eee4cd25b6a8cfd1cb479b6
Melody Huang <yth.melody@gmail.com>
2023-03-18 15:00:18 +0800

    2nd Version MathBranch MyMath

--- GitBranch01/src/app/Main.java
+++ GitBranch01/src/app/Main.java
@@ -5,6 +5,14 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("1st version");
+		MyMath myMath = new MyMath();
+		System.out.println(myMath.add(10, 5));
 	}
 
+}
+
+class MyMath {
+	int add(int a, int b) {
+			return a+b;
+	}
 }
